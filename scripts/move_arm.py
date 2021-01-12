e89a34f58b14871589ca753968070c7d7561c78e
Arthur <arthur.richards@bristol.ac.uk>
2021-01-12 17:08:08 +0000

    Minor tweaks

--- scripts/move_arm.py
+++ scripts/move_arm.py
@@ -1,25 +1,32 @@
 #!/usr/bin/env python
 import sys
 import roslib
-roslib.load_manifest('farscope_group_proj')
 import rospy
 from math import pi
-from trajectory_msgs.msg import JointTrajectory,JointTrajectoryPoint
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from control_msgs.msg import FollowJointTrajectoryGoal, FollowJointTrajectoryAction
 from actionlib import SimpleActionClient
+roslib.load_manifest('farscope_group_proj')
+
 
 class ArmMover:
     """Client to move the robot arm"""
     def __init__(self):
-        self.client = SimpleActionClient("/arm_controller/follow_joint_trajectory", FollowJointTrajectoryAction)
+        self.client = SimpleActionClient("/arm_controller/follow_joint_trajectory",
+                                         FollowJointTrajectoryAction)
         rospy.loginfo("waiting to connect...")
         self.client.wait_for_server()
         rospy.loginfo("connected! ")
-        self.joint_names = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
+        self.joint_names = ['shoulder_pan_joint',
+                            'shoulder_lift_joint',
+                            'elbow_joint',
+                            'wrist_1_joint',
+                            'wrist_2_joint',
+                            'wrist_3_joint']
 
-    def move(self, shoulder_lift_cmd_in = -pi/4.0, elbow_cmd_in = pi/2.0, duration_in = 5.0):
-        wrist_1_cmd = (-1.0)*(shoulder_lift_cmd_in + elbow_cmd_in) # keep it level
-        wrist_2_cmd = 0.5*pi # straight along arm
+    def move(self, shoulder_lift_cmd_in=-pi/4.0, elbow_cmd_in=pi/2.0, duration_in=5.0):
+        wrist_1_cmd = (-1.0)*(shoulder_lift_cmd_in + elbow_cmd_in)  # keep it level
+        wrist_2_cmd = 0.5*pi  # straight along arm
         g = FollowJointTrajectoryGoal()
         g.trajectory = JointTrajectory()
         g.trajectory.joint_names = self.joint_names
@@ -29,19 +36,19 @@ class ArmMover:
         p1.time_from_start = rospy.Duration(duration_in)
         g.trajectory.points.append(p1)
         self.client.send_goal(g)
-        rospy.loginfo( "sent the goal")
-        rospy.loginfo( "waiting to get there")
+        rospy.loginfo("Sent goal and waiting for arm")
         self.client.wait_for_result()
-        rospy.loginfo( "got there")
+        rospy.loginfo("Arm move complete")
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     rospy.init_node("move_arm")
     m = ArmMover()
-    if len(sys.argv)>=2:
+    if len(sys.argv) >= 2:
         shoulder_lift_cmd = float(sys.argv[1])
     else:
         shoulder_lift_cmd = -pi/4.0
-    if len(sys.argv)>=3:
+    if len(sys.argv) >= 3:
         elbow_cmd = float(sys.argv[2])
     else:
         elbow_cmd = pi/2.0
