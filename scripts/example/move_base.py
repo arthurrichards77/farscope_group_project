cdf79f3ba31bd05aa3c1863f31b8156620fbefdc
farscope <farscope-cdt@bristol.ac.uk>
2021-02-25 08:31:06 +0000

    move_base utility now takes move duration argument

--- scripts/example/move_base.py
+++ scripts/example/move_base.py
@@ -4,17 +4,26 @@ import sys
 from farscope_group_project.farscope_robot_utils import BaseDriver
 
 rospy.init_node('move_base', anonymous=True)
+
 if len(sys.argv) >= 2:
     x_trans = float(sys.argv[1])
 else:
     x_trans = 0.0
+
 if len(sys.argv) >= 3:
     y_trans = float(sys.argv[2])
 else:
     y_trans = 0.0
+
 if len(sys.argv) >= 4:
     z_rot = float(sys.argv[3])
 else:
     z_rot = 0.0
+
+if len(sys.argv) >= 5:
+    move_time = float(sys.argv[4])
+else:
+    move_time = 1.0
+
 d = BaseDriver()
-d.move(x_trans, y_trans, z_rot)
+d.move(x_trans, y_trans, z_rot, move_time)
